222ee6395d4111b5611cfe2e9df53b264b4fd2f1
haon7663 <89632611+haon7663@users.noreply.github.com>
2023-04-11 23:16:39 +0900

    20230411
    w

--- ZEMJAM_Project/Assets/Scripts/UI/OptionsManager.cs
+++ ZEMJAM_Project/Assets/Scripts/UI/OptionsManager.cs
@@ -7,11 +7,24 @@ public class OptionsManager : MonoBehaviour
 {
     public GameObject m_Right;
     public GameObject m_Left;
+
     public Text m_Explain;
     public Image m_Image;
 
-    public GameObject m_Options;
+    public GameObject m_Options;   
+    public Sprite[] sprites;
+    [TextArea]
+    public string[] explain;
+    private int count;
 
+    public void OnRightLeft(int a)
+    {
+        count += a;
+        m_Explain.text = explain[count];
+        m_Image.sprite = sprites[count];
+        m_Right.SetActive(count < 5);
+        m_Left.SetActive(count > 0);
+    }
     public void OnOptionSetting()
     {
         m_Options.SetActive(!m_Options.activeSelf);
