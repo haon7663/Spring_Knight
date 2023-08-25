0bc92e9ea3cdc2f8e4406b8259787f0ca7b92aa0
haon7663 <89632611+haon7663@users.noreply.github.com>
2023-08-25 18:34:26 +0900

    StageManager

--- ZEMJAM_Project/Assets/Scripts/StageManager.cs
+++ ZEMJAM_Project/Assets/Scripts/StageManager.cs
@@ -7,12 +7,12 @@ public class StageManager : MonoBehaviour
 {
     public void GameStart()
     {
-        GameManager.Gm.m_EnemySummonCount = 4;
-        GameManager.Gm.m_Score = 0;
-        GameManager.Gm.TileSize = 6;
-        GameManager.Gm.MaxPower = 2;
-        GameManager.Gm.paze = 0;
-        GameManager.Gm.Managerhp = 3;
+        GameManager.Inst.m_EnemySummonCount = 4;
+        GameManager.Inst.m_Score = 0;
+        TileManager.Inst.tileSize = 6;
+        GameManager.Inst.MaxPower = 2;
+        GameManager.Inst.paze = 0;
+        GameManager.Inst.Managerhp = 3;
         StartCoroutine(MoveScene());
     }
     IEnumerator MoveScene()
