4518255e197cde78dfeb4adca118899b8c598392
haon7663 <89632611+haon7663@users.noreply.github.com>
2023-08-30 00:02:31 +0900

    Update PlayerSpriteRenderer.cs

--- ZEMJAM_Project/Assets/Users/Scripts/Player/PlayerSpriteRenderer.cs
+++ ZEMJAM_Project/Assets/Users/Scripts/Player/PlayerSpriteRenderer.cs
@@ -7,7 +7,7 @@ public class PlayerSpriteRenderer : MonoBehaviour
     [Header("SpriteRenderers")]
     [SerializeField] SpriteRenderer m_SpriteRenderer;
     [SerializeField] SpriteRenderer m_SwordSpriteRenderer;
-    [SerializeField] SpriteRenderer m_AfterSpriteRenderer;
+    [SerializeField] SpriteRenderer m_SpinSpriteRenderer;
 
     [Header("Components")]
     [SerializeField] Movement m_Movement;
@@ -17,7 +17,10 @@ public class PlayerSpriteRenderer : MonoBehaviour
     [SerializeField] Material defaultMaterial;
     [SerializeField] Material whiteMaterial;
 
-    float hitTime;
+    [Header("AfterImage")]
+    [SerializeField] GameObject afterImagePrf;
+
+    float hitTimer, afterTimer;
 
     void Update()
     {
@@ -33,17 +36,47 @@ public class PlayerSpriteRenderer : MonoBehaviour
                 m_SpriteRenderer.flipX = false;
         }
         m_SwordSpriteRenderer.flipX = m_SpriteRenderer.flipX;
+
+        if (m_Movement.count > 0)
+        {
+            var bounceCount = m_Movement.bouncedCount;
+            afterTimer += Time.deltaTime;
+            if (afterTimer > 0.05f)
+            {
+                if (bounceCount >= 3)
+                {
+                    SummonAfterImage(m_SpriteRenderer);
+                    if (bounceCount >= 10)
+                        SummonAfterImage(m_SpinSpriteRenderer);
+                    else if (bounceCount >= 7)
+                        SummonAfterImage(m_SwordSpriteRenderer);
+
+                    afterTimer = 0;
+                }
+            }
+        }
     }
 
+    void SummonAfterImage(SpriteRenderer spriteRenderer)
+    {
+        SpriteRenderer afterImage = Instantiate(afterImagePrf, transform.position, Quaternion.identity).GetComponent<SpriteRenderer>();
+        afterImage.sprite = spriteRenderer.sprite;
+        afterImage.flipX = spriteRenderer.flipX;
+    }
     void SetMaterial()
     {
-        m_SpriteRenderer.material = hitTime > 0 ? whiteMaterial : defaultMaterial;
-        if (hitTime > 0) hitTime -= Time.deltaTime;
+        m_SpriteRenderer.material = hitTimer > 0 ? whiteMaterial : defaultMaterial;
+        if (hitTimer > 0) hitTimer -= Time.deltaTime;
+    }
+
+    void CompareFlip(Transform target)
+    {
+        m_SpriteRenderer.flipX = target.position.x > transform.position.x;
     }
 
     public IEnumerator GracePerioding()
     {
-        hitTime = 0.1f;
+        hitTimer = 0.1f;
         for (int i = 0; i < 5; i++)
         {
             m_SpriteRenderer.color = new Color(1, 1, 1, m_SpriteRenderer.color.a == 0.4f ? 0.8f : 0.4f);
