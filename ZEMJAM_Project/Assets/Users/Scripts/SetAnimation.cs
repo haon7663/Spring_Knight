555a7cfcdf53520e625955491d467925374c749b
haon7663 <89632611+haon7663@users.noreply.github.com>
2023-08-30 00:02:27 +0900

    Update SetAnimation.cs

--- ZEMJAM_Project/Assets/Users/Scripts/SetAnimation.cs
+++ ZEMJAM_Project/Assets/Users/Scripts/SetAnimation.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class SetAnimation : MonoBehaviour
 {
-    [SerializeField] Animator m_SwordAnimator;
-    private Animator m_Animator;
-    private Collison m_Collison;
-    private Movement m_Movement;
+    Animator m_Animator;
+    Animator m_SwordAnimator;
+    Collison m_Collison;
+    Movement m_Movement;
 
-    private void Start()
+    void Start()
     {
         m_Animator = GetComponent<Animator>();
         m_Collison = GetComponent<Collison>();
         m_Movement = GetComponent<Movement>();
+
+        m_SwordAnimator = transform.GetChild(0).GetComponent<Animator>();
     }
-    private void Update()
+    void Update()
     {
         m_Animator.SetBool("onWall", m_Collison.onRight || m_Collison.onLeft);
         m_Animator.SetBool("onTopWall", m_Collison.onUp);
@@ -23,5 +25,17 @@ public class SetAnimation : MonoBehaviour
         m_Animator.SetBool("onMove", m_Movement.count > 0);
         m_Animator.SetBool("isAttack", m_Movement.isAttacking);
         m_SwordAnimator.SetBool("isAttack", m_Movement.isAttacking);
+    }
+
+    public void AttackTrigger()
+    {
+        m_Animator.SetTrigger("attack");
+        m_SwordAnimator.SetTrigger("swing");
+    }
+
+    public void Spin(bool value)
+    {
+        m_Animator.SetBool("isSpin", value);
+        if(value) m_Animator.SetTrigger("spin");
     }
 }
