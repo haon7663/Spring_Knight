6e1501f15b36559a79fb4f628e5064084f79f701
haon7663 <89632611+haon7663@users.noreply.github.com>
2023-08-30 00:02:21 +0900

    Update Sword.cs

--- ZEMJAM_Project/Assets/Users/Scripts/Sword.cs
+++ ZEMJAM_Project/Assets/Users/Scripts/Sword.cs
@@ -4,25 +4,44 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
-    private Movement mMovement;
-    private Animator mAnimator;
-    private Collison mCollison;
+    SpriteRenderer m_SpriteRenderer;
+    SetAnimation m_SetAnimation;
 
-    private SpriteRenderer mSpriteRenderer;
-
-    private AudioSource mAudioSource;
-
+    [SerializeField] GameObject spinSword;
+    [SerializeField] LayerMask enemyLayer;
 
     private void Start()
     {
-        mAudioSource = GetComponent<AudioSource>();
-        mMovement = GetComponentInParent<Movement>();
-        mAnimator = GetComponent<Animator>();
-        mCollison = GetComponentInParent<Collison>();
-        mSpriteRenderer = GetComponent<SpriteRenderer>();
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        m_SetAnimation = GetComponentInParent<SetAnimation>();
     }
     private void Update()
     {
-        mSpriteRenderer.enabled = mMovement.count > 0;
+        var movement = Movement.Inst;
+        m_SpriteRenderer.enabled = movement.count > 0;
+
+        if (GameManager.Inst.isSetting || HealthManager.Inst.curhp <= 0) return;
+
+        if (movement.bouncedCount >= 10 && movement.count > 0)
+        {
+            if (!spinSword.activeSelf)
+            {
+                m_SetAnimation.Spin(true);
+                spinSword.SetActive(true);
+            }
+            spinSword.transform.Rotate(0, 0, 2000 * Time.deltaTime);
+
+            Collider2D enemy = Physics2D.OverlapCircle(transform.position, 2, enemyLayer);
+            if (enemy)
+            {
+                CinemachineShake.Instance.ShakeCamera(7, 0.5f);
+                enemy.transform.GetComponent<EnemyDefence>().OnDamage();
+            }
+        }
+        else if (spinSword.activeSelf)
+        {
+            spinSword.SetActive(false);
+            m_SetAnimation.Spin(false);
+        }
     }
 }
