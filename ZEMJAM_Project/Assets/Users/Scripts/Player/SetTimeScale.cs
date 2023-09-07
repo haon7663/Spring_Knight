d333d8e8f28f1a140ea1c9054cc23f8c4040b395
haon7663 <89632611+haon7663@users.noreply.github.com>
2023-09-07 23:21:40 +0900

    Update SetTimeScale.cs

--- ZEMJAM_Project/Assets/Users/Scripts/Player/SetTimeScale.cs
+++ ZEMJAM_Project/Assets/Users/Scripts/Player/SetTimeScale.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class SetTimeScale : MonoBehaviour
 {
-    Movement m_Movement;
     Rigidbody2D m_Rigidbody2D;
 
     [SerializeField] Vector3[] offset;
@@ -14,15 +13,15 @@ public class SetTimeScale : MonoBehaviour
 
     void Start()
     {
-        m_Movement = GetComponent<Movement>();
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     void LateUpdate()
     {
+        if (Movement.Inst.isIgnoreCollison) return;
         var setTime = defaultTimeScale;
         for (int i = 0; i < offset.Length; i++)
-            if (Physics2D.Raycast(transform.position + offset[i], m_Rigidbody2D.velocity, 10000, enemyLayer) && m_Movement.count > 0)
+            if (Physics2D.Raycast(transform.position + offset[i], m_Rigidbody2D.velocity, 10000, enemyLayer) && Movement.Inst.count > 0)
                 setTime = 0.4f;
 
         Time.timeScale = Mathf.Lerp(Time.timeScale, setTime, Time.deltaTime * 9);
