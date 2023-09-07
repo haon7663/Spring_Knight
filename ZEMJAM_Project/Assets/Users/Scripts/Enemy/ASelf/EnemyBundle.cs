721335b25a96fa08b5740b19828a8ef735ec4232
haon7663 <89632611+haon7663@users.noreply.github.com>
2023-09-07 23:20:39 +0900

    EnemyBundle

---
+++ ZEMJAM_Project/Assets/Users/Scripts/Enemy/ASelf/EnemyBundle.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBundle : MonoBehaviour
+{
+    public EnemyRigidbody rigid;
+    public EnemyDefence defence;
+    public EnemySprite sprite;
+}
