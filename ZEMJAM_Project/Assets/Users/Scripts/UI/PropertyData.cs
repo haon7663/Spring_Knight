ba526d215e02f25b2d6d7d256df8a095129fea74
haon7663 <89632611+haon7663@users.noreply.github.com>
2023-09-22 15:25:29 +0900

    Update PropertyData.cs

--- ZEMJAM_Project/Assets/Users/Scripts/UI/PropertyData.cs
+++ ZEMJAM_Project/Assets/Users/Scripts/UI/PropertyData.cs
@@ -6,5 +6,16 @@ using UnityEngine.UI;
 public class PropertyData : MonoBehaviour
 {
     public Text explainText;
-    public Image image;
+    public Image spriteImage;
+
+    void Start()
+    {
+        Property property = PropertiesManager.Inst.GetProperty();
+
+        float value = property.value * (property.timeType == TimeType.COUNT ? property.curCount : 1);
+        string explain = property.explain.Replace("!", value.ToString());
+
+        explainText.text = explain;
+        spriteImage.sprite = property.sprite;
+    }
 }
